42234a279f41e34d146ee09b0981727b19ecce60
SireneNouira <sirene.nouira16@gmail.com>
2024-11-12 16:57:45 +0100

    5

---
+++ script.js
@@ -0,0 +1,22 @@
+//EXERCICE 4
+
+// for (i = 0; i < 21; i++) {
+//   if (i % 2 === 0) console.log(i);
+// }
+
+
+//EXERCICE 5
+
+// function nombreVoyelles(mot) {
+//     let voyelles = "aAeEiIoOuU";
+//     let result= 0;
+//     for (let i = 0; i < mot.length; i++) {
+//         if (voyelles.indexOf(mot[i]) !== -1) {
+//             result+=1;
+//         }
+//     }
+//     return result;
+// }
+
+// console.log(nombreVoyelles("mot"));
+// console.log(nombreVoyelles("motaeiiii"));
